3cac751b389a5d529887f99d1d3e02c1fc0a9b1b
Rorybabory <lambertrory123@gmail.com>
2022-04-09 17:08:29 -0700

    fixed auto mistakes

--- src/vision.py
+++ src/vision.py
@@ -2,50 +2,43 @@ from cscore import CameraServer
 from networktables import NetworkTables
 import time
 import logging
-import math
+
 import cv2
 import numpy as np
-def toRad(val):
-    return val*(3.1415*180)
-def getDistance(pos_y):
-    res_h = 120.0
-    ay = -(pos_y - (res_h/2)) / (res_h/2)
-    dy = 6.0
-    dz = 8.0
-    fov = 2*math.tan((0.5*dy)/dz)
-    pitch = (ay/2.0)*fov
-    target_h = 104.02
-    camera_h = 35.0
-    camera_a = toRad(28.0)
-    distance = (target_h-camera_h)/(math.tan((camera_a+pitch)))
-    print("distance is: " + str(distance) + " ay is " + str(ay) + " fov is: " + str(fov))
 
 
 NetworkTables.initialize(server='roborio-2035-frc.local')
 
-sd = NetworkTables.getTable("SmartDashboard")
+rpi = NetworkTables.getTable("RPi")
 
 
 logging.basicConfig(level=logging.DEBUG)
 cs = CameraServer.getInstance()
 cs.enableLogging()
-camera = cs.startAutomaticCapture(dev=1)
-usb1 = cs.startAutomaticCapture(dev=0)
-
-camera.setResolution(640, 480)
-usb1.setResolution(640, 480)
-
+camera = cs.startAutomaticCapture()
+camera.setResolution(160, 120)
 
 sink = cs.getVideo()
 
-img = np.zeros(shape=(120, 160, 3),dtype=np.uint8)
-output = cs.putVideo("Camera View", 160, 120)
+img = np.zeros(shape=(240, 320, 3),dtype=np.uint8)
+output = cs.putVideo("Name", 320, 240)
+capture = cv2.VideoCapture('http://10.20.35.210:5800/image.mjpg')
 while True:
-    time, input_img = sink.grabFrame(img)
-    distance = sd.getNumber("Target Distance", -2.0)
-    input_img = cv2.putText(input_img, str(distance), (0,0), cv2.FONT_HERSHEY_SIMPLEX, 
-                   1, (255,0,0), 1, cv2.LINE_AA)
-    if time == 0:
-        continue
+    ret, input_img = capture.read() 
     
+    hsv_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2HSV)
+    binary_img = cv2.inRange(hsv_img, (71,119,138), (102,227,255))
+    _, contours, _ = cv2.findContours(binary_img,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if (len(contours) > 0):
+        largest = contours[0]
+        for contour in contours:
+            if cv2.contourArea(contour) > cv2.contourArea(largest):
+                largest = contour
+        rect = cv2.minAreaRect(largest)
+        center, _, _ = rect
+        center_x, center_y = center
+        rpi.putNumber('tx', ((center_x/150.0)-0.5)*2.0)
+        rpi.putNumber('ty', ((center_y/108.0)-0.5)*2.0)
+        rpi.putNumber('ta', np.sum(binary_img == 255)/(160.0*120.0))
     output.putFrame(input_img)
+capture.release()
